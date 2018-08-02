e0e21fc44a21a529b22858a741c8a5a75bf0b59d
Krishan Mistry <kvjmistry@gmial.com>
2018-08-02 13:07:03 +0100

    Added Some more histograms to the mix

--- AnalyseNueKinematics.C
+++ AnalyseNueKinematics.C
@@ -9,9 +9,35 @@
 #include <iostream>
 #include <fstream>
 
+
+
 // Main Function
 void AnalyseNueKinematics() {
-    		
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //                                                  Initialize
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Create Histograms
+    // Nue All
+    TH2D*   hNue_E_vs_Theta = new TH2D("Nue_E_vs_Theta_All","Nue_E_vs_Theta_All; Energy [GeV]; Theta [degrees]",15., 0., 7. , 10., 0., 180);
+    TH2D*   hNue_E_vs_Phi   = new   TH2D("Nue_E_vs_Phi_All","Nue_E_vs_Phi_All; Energy [GeV]; Phi [degrees]",10., 0., 10. , 10., -180., 180);
+
+    TH1D* 	hNue_Energy = new TH1D("Nue_Energy_All","Nue_Energy_All; E [GeV]; Events",50., 0., 5);
+    TH1D* 	hNue_Theta = new TH1D("Nue_Theta_All","Nue_Theta_All; Theta [Degrees]; Events", 100., 0., 180);
+    TH1D* 	hNue_Phi = new TH1D("Nue_Phi_All","Nue_Phi_All; Phi [Degrees]; Events", 10., -180., 180);
+
+    // Electron all
+    TH2D*   helectron_E_vs_Theta = new TH2D("electron_E_vs_Theta_All","electron_E_vs_Theta_All; Energy [GeV]; Theta [degrees]",20., 0., 10. , 10., 0., 180);
+    TH2D*   helectron_E_vs_Phi   = new TH2D("electron_E_vs_Phi_All","electron_E_vs_Phi_All; Energy [GeV]; Phi [degrees]",20., 0., 10. , 10., -180., 180);
+  
+    TH1D* 	helectron_Energy = new TH1D("electron_Energy_All","electron_Energy_All; E [GeV]; Events",50., 0., 5.);
+    TH1D* 	helectron_Theta  = new TH1D("electron_Theta_All","electron_Theta_All; Theta [Degrees]; Events", 10., 0., 180);
+    TH1D* 	helectron_Phi    = new TH1D("electron_Phi_All","electron_Phi_All; Phi [Degrees]; Events", 10., -180., 180);
+
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //                                                  Read In TTree
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
 	// Open the NueKinematics File
 	TFile *fileIN = TFile::Open("NueKinematics.root");
 	if (fileIN == 0) {
@@ -20,26 +46,85 @@ void AnalyseNueKinematics() {
 	  return;
 	}
 
-    double Total_events = 0;
-	double Passed_Events = 0;
-
 	// Create the tree reader and its data containers
 	TTreeReader myReader("microboonewvtof/EventTree", fileIN);
 
-	TTreeReaderValue<std::vector<int>> PDGRV(myReader, "PDG"); 					// Load in the PDG variable
-	TTreeReaderValue<std::vector<double>> NueEnergyRV(myReader, "NueEnergy"); 	// Load in the NueEnergy variable
+	TTreeReaderValue<int> PDGRV(myReader, "PDG"); 					// Load in the PDG variable
+	TTreeReaderValue<double> NueEnergyRV(myReader, "NueEnergy"); 	// Load in the NueEnergy variable
+    TTreeReaderValue<double> NueThetaRV(myReader, "NueTheta"); 	    // Load in the NueTheta variable
+    TTreeReaderValue<double> NuePhiRV(myReader, "NuePhi"); 	        // Load in the NuePhi variable
 
-	// Loop over all entries of the TTree or TChain.
+    TTreeReaderValue<double> ElectronEnergyRV(myReader, "ElectronEnergy"); 	// Load in the ElectronEnergy variable
+    TTreeReaderValue<double> ElectronThetaRV(myReader, "ElectronTheta"); 	// Load in the ElectronTheta variable
+    TTreeReaderValue<double> ElectronPhiRV(myReader, "ElectronPhi"); 	    // Load in the ElectronPhi variable
+
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //                                         Loop over all entries of the TTree or TChain.
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
 	while (myReader.Next()) {
-		if ( (*PDGRV)[0] >= 1  ) Total_events++; 	
 
-		if ( (*NueEnergyRV)[0] == 1   ) Passed_Events++; 
+        hNue_E_vs_Theta->Fill(*NueEnergyRV, *NueThetaRV );
+        hNue_E_vs_Phi->Fill(*NueEnergyRV, *NuePhiRV);
+
+
+		hNue_Energy->Fill(*NueEnergyRV); 
+        hNue_Theta->Fill(*NueThetaRV); 
+        hNue_Phi->Fill(*NuePhiRV); 
+        
+
+        helectron_E_vs_Theta->Fill(*ElectronEnergyRV, *ElectronThetaRV );
+        helectron_E_vs_Theta->Fill(*ElectronEnergyRV, *ElectronPhiRV);
+
+        helectron_Energy->Fill(*ElectronEnergyRV);
+        helectron_Theta->Fill(*ElectronThetaRV);
+        helectron_Phi->Fill(*ElectronPhiRV);
 
 	}
 
-    std::cout << "\nTest Output : " << Total_events << std::endl;
-    std::cout << "\nTest Output2 : " << Passed_Events << std::endl;
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //                                                  Make Histograms
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-}
+    // Nue
+    auto cNue_E_vs_Theta = new TCanvas();
+    hNue_E_vs_Theta->Draw("COLZ,TEXT00");
+    
+    auto cNue_E_vs_Phi = new TCanvas();
+    hNue_E_vs_Phi->Draw("COLZ,TEXT00");
+    
+    auto cNue_Energy = new TCanvas();
+    hNue_Energy->Draw("HIST,TEXT00");
+
+    auto cNue_Theta = new TCanvas();
+    hNue_Theta->Draw("HIST,TEXT00");
+
+    auto cNue_Phi = new TCanvas();
+    hNue_Phi->Draw("HIST,TEXT00");
+
+    
+    // Electron
+    auto celectron_E_vs_Theta = new TCanvas();
+    helectron_E_vs_Theta->Draw("COLZ,TEXT00");
+    
+    auto celectron_E_vs_Phi = new TCanvas();
+    helectron_E_vs_Phi->Draw("COLZ,TEXT00");
+    
+    auto celectron_Energy = new TCanvas();
+    helectron_Energy->Draw("HIST,TEXT00");
+
+    auto celectron_Theta = new TCanvas();
+    helectron_Theta->Draw("HIST,TEXT00");
+
+    auto celectron_Phi = new TCanvas();
+    helectron_Phi->Draw("HIST,TEXT00");
+
+
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //                                                     END
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+} // END MAIN FUNCTION
 
 
